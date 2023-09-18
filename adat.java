1ae93ab2a76c73a090a3e40be3a8cd552c648365
szt2GémesSzabolcs <115459925+Szabi20@users.noreply.github.com>
2023-09-18 16:30:42 +0200

    Add files via upload

---
+++ adat.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class adat {
+    public static void main(String[] args){
+    String nev="Szabi";
+    int eletkor=21;
+    double magsag=183.5;
+    boolean házas=false;
+    System.out.println(nev);
+    System.out.println(eletkor);
+    System.out.println(magsag);
+    System.out.println(házas);
+    
+    }
+    
+}
